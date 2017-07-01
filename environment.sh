8beb029a1c545ff111bd6cfa090484f0c472d81f
Juan Miguel Mora <jmilet@gmail.com>
2017-07-01 12:17:23 +0200

    Move file to a better location

--- environment.sh
+++ environment.sh
@@ -3,11 +3,11 @@ export GOPATH=$KUBPRJ/go
 export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$HOME/kubernetes
 
-export GOOS=darwin
-export GOARCH=amd64
-
-#export GOOS=linux
+#export GOOS=darwin
 #export GOARCH=amd64
+
+export GOOS=linux
+export GOARCH=amd64
 
 #export GOOS=freebsd
 #export GOARCH=amd64
