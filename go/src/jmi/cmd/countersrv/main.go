c6ed6c75851ceb12f9b3f02635d3bb83ddcb6bc1
Juan Miguel Mora <jmilet@gmail.com>
2017-07-01 13:26:53 +0200

    Add return hostname

--- go/src/jmi/cmd/countersrv/main.go
+++ go/src/jmi/cmd/countersrv/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"jmi/counter"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -17,6 +19,14 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		input <- string(r.URL.Path)
+		hostName, err := os.Hostname()
+		if err != nil {
+			log.Fatal("Error getting hostname")
+		}
+
+		hostName = fmt.Sprintf(`{"Hostname:" "%s"}`+"\n", hostName)
+
+		w.Write([]byte(hostName))
 	})
 
 	http.ListenAndServe(":9092", nil)
